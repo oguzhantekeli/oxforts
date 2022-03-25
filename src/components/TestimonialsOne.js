3da4c2178ebab335965901eee372c488714410ab
oguzhan <oguzhantekeli@gmail.com>
2022-03-25 16:01:51 +0300

    TestimonialsOne

---
+++ src/components/TestimonialsOne.js
@@ -0,0 +1,162 @@
+import React from "react";
+import {
+  qoute_1_1,
+  team_1_1,
+  team_1_2,
+  team_1_3,
+  team_1_4,
+  team_1_5,
+  team_1_6,
+} from "../images/images";
+const TestimonialsOne = () => {
+  return (
+    <>
+      <section className="testimonials-one  ">
+        <div className="container">
+          <div className="block-title text-center">
+            <h2 className="block-title__title">
+              What our students <br />
+              have to say
+            </h2>
+          </div>
+          <div className="testimonials-one__carousel owl-carousel owl-theme">
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority.
+                </p>
+                <img src={team_1_1} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Anne Hall</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority have suffered alteration in some form.
+                </p>
+                <img src={team_1_2} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Andre Obrien</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority have suffered alteration in some form, by
+                  injected humour.
+                </p>
+
+                <img src={team_1_3} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Shane Vasquez</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority.
+                </p>
+                <img src={team_1_4} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Maud Lee</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority have suffered alteration in some form.
+                </p>
+                <img src={team_1_5} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Barbara Kennedy</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority have suffered alteration in some form, by
+                  injected humour.
+                </p>
+
+                <img src={team_1_6} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Duane Carter</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority.
+                </p>
+                <img src={team_1_1} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Sally Green</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority have suffered alteration in some form.
+                </p>
+                <img src={team_1_2} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Iva Santos</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+            <div className="item">
+              <div className="testimonials-one__single">
+                <div className="testimonials-one__qoute">
+                  <img src={qoute_1_1} alt="" />
+                </div>
+                <p className="testimonials-one__text">
+                  There are many variations of passages of lore ipsu available
+                  but the majority have suffered alteration in some form, by
+                  injected humour.
+                </p>
+
+                <img src={team_1_3} alt="" className="testimonials-one__img" />
+                <h3 className="testimonials-one__name">Max Burns</h3>
+                <p className="testimonials-one__designation">Student</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TestimonialsOne;
