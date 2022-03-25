354b89214351a0afb877c5bd4eaaeb2bbe0cb2b8
oguzhan <oguzhantekeli@gmail.com>
2022-03-25 17:05:53 +0300

    aboutpage

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import InnerBanner from "../components/InnerBanner";
+import AboutOne from "../components/AboutOne";
+import TeamOne from "../components/TeamOne";
+import VideoOne from "../components/VideoOne";
+import BrandOne from "../components/BrandOne";
+import TestimonialsOne from "../components/TestimonialsOne";
+import CtaOne from "../components/CtaOne";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <InnerBanner title={"About"} />
+      <AboutOne />
+      <TeamOne />
+      <VideoOne />
+      <BrandOne />
+      <TestimonialsOne />
+      <CtaOne />
+    </>
+  );
 };
 
 export default About;
