df68819de5cfadda0f8cb37057c3a5922f437fec
oguzhan <oguzhantekeli@gmail.com>
2022-03-25 17:09:19 +0300

    BecomeTeacher page

--- src/pages/BecomeTeacher.js
+++ src/pages/BecomeTeacher.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import InnerBanner from "../components/InnerBanner";
+import BecomeTeacherComp from "../components/BecomeTeacherComp";
+import TeamOne from "../components/TeamOne";
+import CtaOne from "../components/CtaOne";
 const BecomeTeacher = () => {
-  return <div>BecomeTeacher</div>;
+  return (
+    <>
+      <InnerBanner title={"Become Teacher"} />
+      <BecomeTeacherComp />
+      <TeamOne />
+      <CtaOne />
+    </>
+  );
 };
 
 export default BecomeTeacher;
