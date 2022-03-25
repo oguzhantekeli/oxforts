5ccc74aace3bbaac9fcfcb76b869b6a62555713d
oguzhan <oguzhantekeli@gmail.com>
2022-03-25 15:57:52 +0300

    BrandOne

---
+++ src/components/BrandOne.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { brand_1_1 } from "../images/images";
+const BrandOne = () => {
+  return (
+    <>
+      <section className="brand-one  ">
+        <div className="container">
+          <div className="brand-one__carousel owl-carousel owl-theme">
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+            <div className="item">
+              <img src={brand_1_1} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default BrandOne;
