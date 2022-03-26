c96947d46071cc97e22c1a9f83221f7dd8ba3480
oguzhan <oguzhantekeli@gmail.com>
2022-03-26 13:03:40 +0300

    Faq

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import InnerBanner from "../components/InnerBanner";
+import FaqOne from "../components/FaqOne";
 const Faq = () => {
-  return <div>Faq</div>;
+  return (
+    <>
+      <InnerBanner title={"Faq"} />
+      <FaqOne />
+    </>
+  );
 };
 
 export default Faq;
