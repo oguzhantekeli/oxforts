a6828a74afb064ddc70f1c53f51bc259f54046f2
oguzhan <oguzhantekeli@gmail.com>
2022-03-26 13:09:56 +0300

    Pricing

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import InnerBanner from "../components/InnerBanner";
+import PricingOne from "../components/PricingOne";
+import CtaOne from "../components/CtaOne";
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <>
+      <InnerBanner title={"Pricing"} />
+      <PricingOne />
+      <CtaOne />
+    </>
+  );
 };
 
 export default Pricing;
