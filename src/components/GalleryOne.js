00592f420206d498af21fb00aa1c87a2facf52a7
oguzhan <oguzhantekeli@gmail.com>
2022-03-26 18:44:34 +0300

    gallery image popup smmoth scroll

--- src/components/GalleryOne.js
+++ src/components/GalleryOne.js
@@ -1,96 +1,72 @@
-import React from "react";
-import {
-  gallery_1_1,
-  gallery_1_2,
-  gallery_1_3,
-  gallery_1_4,
-  gallery_1_5,
-  gallery_1_6,
-  gallery_1_7,
-  gallery_1_8,
-  gallery_1_9,
-} from "../images/images";
+import React, { useState } from "react";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+
 const GalleryOne = () => {
+  //todo: get image data from store...
+  const images = [
+    "assets/images/gallery-1-1.jpg",
+    "assets/images/gallery-1-2.jpg",
+    "assets/images/gallery-1-3.jpg",
+    "assets/images/gallery-1-4.jpg",
+    "assets/images/gallery-1-5.jpg",
+    "assets/images/gallery-1-6.jpg",
+    "assets/images/gallery-1-7.jpg",
+    "assets/images/gallery-1-8.jpg",
+    "assets/images/gallery-1-9.jpg",
+  ];
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <section className="gallery-one">
         <div className="container">
           <div className="row">
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_1} alt="" />
-                <a href={gallery_1_1} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_2} alt="" />
-                <a href={gallery_1_2} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_3} alt="" />
-                <a href={gallery_1_3} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_4} alt="" />
-                <a href={gallery_1_4} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_5} alt="" />
-                <a href={gallery_1_5} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_6} alt="" />
-                <a href={gallery_1_6} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_7} alt="" />
-                <a href={gallery_1_7} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_8} alt="" />
-                <a href={gallery_1_8} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6">
-              <div className="gallery-one__single">
-                <img src={gallery_1_9} alt="" />
-                <a href={gallery_1_9} className="gallery-one__popup img-popup">
-                  <i className="kipso-icon-plus-symbol"></i>
-                </a>
-              </div>
-            </div>
+            {images.map((item, idx) => {
+              return (
+                <div
+                  className="col-lg-4 col-md-6"
+                  key={idx}
+                  onClick={() => {
+                    setPhotoIndex(idx);
+                    setIsOpen(true);
+                    console.log("open:", isOpen, idx);
+                  }}
+                >
+                  <div className="gallery-one__single">
+                    <img src={item} alt="" />
+                    <span className="gallery-one__popup img-popup">
+                      <i className="kipso-icon-plus-symbol"></i>
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
+
+      {isOpen && (
+        <Lightbox
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => {
+            setIsOpen(false);
+            setPhotoIndex(0);
+            console.log("close:", isOpen);
+          }}
+          onMovePrevRequest={() => {
+            setPhotoIndex((photoIndex + images.length - 1) % images.length);
+            console.log("prev:", photoIndex);
+          }}
+          onMoveNextRequest={() => {
+            setPhotoIndex((photoIndex + 1) % images.length);
+            console.log("next:", photoIndex);
+          }}
+        />
+      )}
     </>
   );
 };
