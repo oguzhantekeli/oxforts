6fa3b3bed6e674c58c737f5d56d7debc084b1e61
oguzhan <oguzhantekeli@gmail.com>
2022-03-26 13:16:47 +0300

    Teachers and CtaTwo

--- src/pages/Teachers.js
+++ src/pages/Teachers.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import InnerBanner from "../components/InnerBanner";
+import TeamOne from "../components/TeamOne";
+import CtaTwo from "../components/CtaTwo";
 const Teachers = () => {
-  return <div>Teachers</div>;
+  return (
+    <>
+      <InnerBanner title={"Teachers"} />
+      <TeamOne />
+      <CtaTwo />
+    </>
+  );
 };
 
 export default Teachers;
