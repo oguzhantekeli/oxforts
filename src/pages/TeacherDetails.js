3c71fb7cbc12b6ef449fd2a6a261945c1db188c2
oguzhan <oguzhantekeli@gmail.com>
2022-03-26 13:12:56 +0300

    TeacherDetails

--- src/pages/TeacherDetails.js
+++ src/pages/TeacherDetails.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import InnerBanner from "../components/InnerBanner";
+import TeamDetailsComp from "../components/TeamDetailsComp";
+import CourseOneTitle from "../components/CourseOneTitle";
+import CourseOneDetails from "../components/CourseOneDetails";
 const TeacherDetails = () => {
-  return <div>TeacherDetails</div>;
+  return (
+    <>
+      <InnerBanner title={"Teacher Details"} />
+      <TeamDetailsComp />
+      <CourseOneTitle />
+      <CourseOneDetails />
+    </>
+  );
 };
 
 export default TeacherDetails;
