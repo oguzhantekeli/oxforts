4cc32a229dad65575fef1a5b454c7b6af9d3de46
oguzhan <oguzhantekeli@gmail.com>
2022-03-26 16:20:29 +0300

    SearchPopup

--- src/components/SearchPopup.js
+++ src/components/SearchPopup.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchPopup = () => {
+  const closePopup = () => {
+    const searchPopup = document.querySelector(".search-popup");
+    searchPopup.classList.toggle("active");
+  };
+  const [searchTerm, setSearchTerm] = useState({ term: "" });
+  const { term } = searchTerm;
+
+  const onChange = (e) => {
+    setSearchTerm((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const submitTerm = { term };
+    console.log(submitTerm);
+    setSearchTerm({ term: "" });
+    closePopup();
+  };
   return (
     <>
       <div className="search-popup">
-        <div className="search-popup__overlay custom-cursor__overlay">
+        <div
+          className="search-popup__overlay custom-cursor__overlay"
+          onClick={closePopup}
+        >
           <div className="cursor"></div>
           <div className="cursor-follower"></div>
         </div>
         <div className="search-popup__inner">
-          <form action="#" className="search-popup__form">
+          <form onSubmit={onSubmit} className="search-popup__form">
             <input
               type="text"
-              name="search"
-              placeholder="Type here to Search...."
+              name="term"
+              placeholder="Ara.."
+              onChange={onChange}
+              value={term}
             />
             <button type="submit">
               <i className="kipso-icon-magnifying-glass"></i>
